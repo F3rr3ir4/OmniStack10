9f27056632c7ffafc1763ef206b1fe6b6d8ae855
Leandro Ferreira Netto <leandroferreira757@msn.com>
2020-01-15 09:25:32 -0300

    completed example header.js

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import Header from "./Header";
 
 function App() {
-    return <Header />;
+    return (
+        <>
+            <Header title="Título1" />
+            <Header title="Título3" />
+            <Header title="Título5" />
+        </>
+    );
 }
 
 export default App;
