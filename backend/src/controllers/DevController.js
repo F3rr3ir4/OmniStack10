ba5bcbb470bc424d8a8753ef2f2af8f8cc86ce93
Leandro Ferreira Netto <leandroferreira757@msn.com>
2020-01-14 21:46:45 -0300

    create devController

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,48 @@
+/******************************************
+ *  Author : Leandro Ferreira Netto
+ *  Created On : Tue Jan 14 2020
+ *  File : DevController.js
+ *******************************************/
+const axios = require("axios");
+const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseSringAsArray");
+
+module.exports = {
+    async index(request, response) {
+        const devs = await Dev.find();
+
+        return response.json(devs);
+    },
+    async store(request, response) {
+        const { github_username, techs, latitude, longitude } = request.body;
+
+        let dev = await Dev.findOne({ github_username });
+        if (!dev) {
+            const { name = login, avatar_url, bio } = apiResponse.data;
+
+            console.log(name, avatar_url, bio, github_username);
+
+            const techsArray = parseStringAsArray(techs);
+
+            const location = {
+                type: "Point",
+                coordinates: [longitude, latitude]
+            };
+
+            const dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            });
+        }
+
+        const apiResponse = await axios.get(
+            `http://api.github.com/users/${github_username}`
+        );
+
+        return response.json(dev);
+    }
+};
