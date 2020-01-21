f4171be14a10a62c71b43da875dee40524f0e8a9
Leandro Ferreira Netto <leandroferreira757@msn.com>
2020-01-21 11:10:01 -0300

    Add server http

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require("http");
 const routes = require("./routes");
 
 const app = express();
+const server = http.Server(app);
 
 mongoose.connect(
     "mongodb+srv://ferreira:F3rr31r4321@cluster0-2qq0n.mongodb.net/week10?retryWrites=true&w=majority",
@@ -24,4 +26,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
